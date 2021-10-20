009f9be14e7092da6d847160dd1e4a57fd8d055e
wangwenlin <wangwenlin@innotechx.com>
2021-10-20 22:43:06 +0800

    修改样式6

--- src/router/asyncCom.js
+++ src/router/asyncCom.js
@@ -7,7 +7,7 @@ const MyLoadingComponent = ({ isLoading, error }) => {
         <div
             style={{
                 width: '100%',
-                height: '100%',
+                height: '400px',
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
