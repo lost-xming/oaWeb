b6928f4ceddd0a60b5f3959787efe289e06c094a
wangwenlin <wangwenlin@innotechx.com>
2021-10-20 22:41:18 +0800

    修改样式5

--- src/router/asyncCom.js
+++ src/router/asyncCom.js
@@ -3,8 +3,20 @@ import React, { Component } from 'react';
 import Loadable from 'react-loadable';
 
 const MyLoadingComponent = ({ isLoading, error }) => {
-    console.log(352);
-    return <Spin />;
+    return (
+        <div
+            style={{
+                width: '100%',
+                height: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+            className="loading_box"
+        >
+            <Spin />
+        </div>
+    );
 };
 
 // 过场组件默认采用通用的，若传入了loading，则采用传入的过场组件
